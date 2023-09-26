69bf2303d96d47e1065b1bf3e227aab6ce98bd38
Peronslayer <lopez.alberto1997bt@gmail.com>
2023-09-26 16:32:28 +0200

    adding some stuff

--- src/app/components/userhome/userhome.component.ts
+++ src/app/components/userhome/userhome.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
+import { FirebaseService } from 'src/app/services/firebase.service';
 
 @Component({
   selector: 'app-userhome',
   templateUrl: './userhome.component.html',
   styleUrls: ['./userhome.component.css']
 })
-export class UserhomeComponent {
+export class UserhomeComponent implements OnInit {
 
   user$ = this.authService.currentUser$;
+  users: any
 
-  constructor(private authService: AuthenticationService){}
+  constructor(private authService: AuthenticationService, private firebase: FirebaseService ){}
+
+
+
+  ngOnInit(): void {
+
+    this.firebase.getUsers("https://jsonplaceholder.typicode.com/users").subscribe(data =>{
+    this.users = data
+    console.log(this.users)
+  
+  });
+    
+  }
+
+
+
+
+
 }
